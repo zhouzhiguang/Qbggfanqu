a7cf9bfc830d59a7fa79e2f923549eeb16dd0cb2
qbgg <123456>
2016-12-10 17:31:48 +0800

    第一次提交

--- laiqu/src/main/java/com/qbgg/cenglaicengqu/homepage/activities/KitchenDetailsActivity.java
+++ laiqu/src/main/java/com/qbgg/cenglaicengqu/homepage/activities/KitchenDetailsActivity.java
@@ -1,15 +1,23 @@
 package com.qbgg.cenglaicengqu.homepage.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.netease.nim.uikit.model.ToolBarOptions;
 import com.qbgg.cenglaicengqu.R;
 import com.qbgg.cenglaicengqu.main.acitvities.BaseActivity;
 import com.qbgg.cenglaicengqu.main.autolayout.AutoUtils;
 import com.qbgg.cenglaicengqu.main.util.ThemUtils;
+import com.qbgg.cenglaicengqu.main.util.ToastUtils;
 
 //厨房详情页面
 public class KitchenDetailsActivity extends BaseActivity implements View.OnClickListener {
+    private AppBarLayout kitchen_details_app_bar_layout;
+    private ImageView kitchen_details_share_image, kitchen_details_like,kitchen_details_share;
+    private ToolBarOptions options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +26,59 @@ public class KitchenDetailsActivity extends BaseActivity implements View.OnClick
         setContentView(R.layout.activity_kitchen_details_layout);
         AutoUtils.setSize(this, false, 1080, 1920);// 没有状态栏,设计尺寸的宽高
         AutoUtils.auto(this);
+        options = new ToolBarOptions();
+        options.titleString = "";
+        options.isNeedNavigate = true;
+        options.navigateId = R.mipmap.ic_left_arrows_white;
+        setToolBar(R.id.toolbar, options);
+        Toolbar toolbar = getToolBar();
+
+        toolbar.setNavigationOnClickListener(new NavigationOnClickListener());
+//          getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//          getSupportActionBar().setDisplayShowHomeEnabled(true);
         initView();
         initDate();
         initListener();
     }
 
     private void initView() {
+        // kitchen_details_swiperefreshLayout = findView(R.id.kitchen_details_swiperefreshLayout);
+        kitchen_details_app_bar_layout = findView(R.id.kitchen_details_app_bar_layout);
+        kitchen_details_like = findView(R.id.kitchen_details_like);
+        kitchen_details_share=findView(R.id.kitchen_details_share);
+
     }
 
     private void initDate() {
+//        kitchen_details_swiperefreshLayout.setRefreshEnable(false);
+//        kitchen_details_swiperefreshLayout.setLoadmoreEnable(false);
+        kitchen_details_app_bar_layout.addOnOffsetChangedListener(new AppBarStateChangeListener() {
+            @Override
+            public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                if (state == State.EXPANDED) {
+                    options.navigateId = R.mipmap.ic_left_arrows_white;
+                    setToolBar(R.id.toolbar, options);
+                    getToolBar().setNavigationOnClickListener(new NavigationOnClickListener());
+                    kitchen_details_like.setSelected(false);
+                    ToastUtils.showCenterToast(KitchenDetailsActivity.this, "展开状态");
+                    //展开状态
+                    kitchen_details_like.setImageResource(R.mipmap.ic_like_white);
+                    kitchen_details_share.setImageResource(R.mipmap.ic_share_white);
+                } else if (state == State.COLLAPSED) {
+                    options.navigateId = R.mipmap.ic_left_arrows;
+                    setToolBar(R.id.toolbar, options);
+                    getToolBar().setNavigationOnClickListener(new NavigationOnClickListener());
+                    //折叠状态
+                    ToastUtils.showCenterToast(KitchenDetailsActivity.this, "折叠状态状态");
+                    kitchen_details_like.setImageResource(R.mipmap.ic_like);
+                    kitchen_details_share.setImageResource(R.mipmap.ic_share_black);
+                } else {
 
+                    //中间状态
+                    ToastUtils.showCenterToast(KitchenDetailsActivity.this, "中间状态");
+                }
+            }
+        });
     }
 
     private void initListener() {
@@ -41,8 +92,50 @@ public class KitchenDetailsActivity extends BaseActivity implements View.OnClick
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
 
         }
+    }
+
+
+    private  class  NavigationOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            ToastUtils.showCenterToast(KitchenDetailsActivity.this, "点击返回了哦");
+        }
+    }
+    public abstract class AppBarStateChangeListener implements AppBarLayout.OnOffsetChangedListener {
+
+
+        private State mCurrentState = State.IDLE;
+
+        @Override
+        public final void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+            if (i == 0) {
+                if (mCurrentState != State.EXPANDED) {
+                    onStateChanged(appBarLayout, State.EXPANDED);
+                }
+                mCurrentState = State.EXPANDED;
+            } else if (Math.abs(i) >= appBarLayout.getTotalScrollRange()) {
+                if (mCurrentState != State.COLLAPSED) {
+                    onStateChanged(appBarLayout, State.COLLAPSED);
+                }
+                mCurrentState = State.COLLAPSED;
+            } else {
+                if (mCurrentState != State.IDLE) {
+                    onStateChanged(appBarLayout, State.IDLE);
+                }
+                mCurrentState = State.IDLE;
+            }
+        }
+
+        public abstract void onStateChanged(AppBarLayout appBarLayout, State state);
+    }
+
+    public enum State {
+        EXPANDED,
+        COLLAPSED,
+        IDLE
     }
 }
